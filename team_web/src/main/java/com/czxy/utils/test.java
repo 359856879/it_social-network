ef6b40a01cd008c20ec856788d7f1d0973b20f03
zhouwenjun <359856879@qq.com>
2019-07-29 22:11:00 +0800

    项目组！7-29-

---
+++ team_web/src/main/java/com/czxy/utils/test.java
@@ -0,0 +1,14 @@
+package com.czxy.utils;
+
+/**
+ * @Auther: 传智新星
+ * @Date: 2019/7/29 22:10
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+
+
+        System.out.println("哈哈哈");
+    }
+}
